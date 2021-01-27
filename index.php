4d22007f68a67cdd2552066d194522d27656428e
Brad Hanel <bxh9261@g.rit.edu>
2021-01-26 22:42:20 -0500

    Update index.php

--- index.php
+++ index.php
@@ -16,23 +16,23 @@
 	// this is an indexed array of associative arrays
 	// the associative arrays are jokes -  they have an 'q' key and an 'a' key
 	$jokes = [
-		["q"=>"What do you call a very small valentine?","a"=>"A valen-tiny!"],
-		["q"=>"What did the dog say when he rubbed his tail on the sandpaper?","a"=>"Ruff, Ruff!"],
-		["q"=>"Why don't sharks like to eat clowns?","a"=>"Because they taste funny!"],
-		["q"=>"What did the boy cat say to the girl cat?","a"=>"You're Purr-fect!"],
-		["q"=>"What is a frog's favorite outdoor sport?","a"=>"Fly Fishing!"],
-		["q"=>"I hate jokes about German sausages.","a"=>"Theyre the wurst."],
-		["q"=>"Did you hear about the cheese factory that exploded in France?","a"=>"There was nothing left but de Brie."],
-		["q"=>"Our wedding was so beautiful ","a"=>"Even the cake was in tiers."],
-		["q"=>"Is this pool safe for diving?","a"=>"It deep ends."],
-		["q"=>"Dad, can you put my shoes on?","a"=>"I dont think theyll fit me."],
-		["q"=>"Can February March?","a"=>"No, but April May"],
-		["q"=>"What lies at the bottom of the ocean and twitches?","a"=>"A nervous wreck."],
-		["q"=>"Im reading a book on the history of glue.","a"=>"I just cant seem to put it down."],
-		["q"=>"Dad, can you put the cat out?","a"=>"I didnt know it was on fire."],
-		["q"=>"What did the ocean say to the sailboat?","a"=>"Nothing, it just waved."],
-		["q"=>"What do you get when you cross a snowman with a vampire?","a"=>"Frostbite"]
-	];
+		["q" => "Comment appelez-vous un très petit valentine?", "a" => "Un valen-tiny!"],
+		["q" => "Qu'est-ce que le chien a dit en se frottant la queue sur le papier de verre?", "a" => "Ruff, Ruff!"],
+		["q" => "Pourquoi les requins n'aiment-ils pas manger des clowns?", "a" => "Parce qu'ils ont un goût drôle!"],
+		["q" => "Qu'est-ce que le petit chat a dit à la petite chatte?", "a" => "Vous êtes ronronnement!"],
+		["q" => "Quel est le sport extérieur préféré d'une grenouille?", "a" => "Pêche à la mouche!"],
+		["q" => "Je déteste les blagues sur les saucisses allemandes.", "a" => "Ils sont la wurst."],
+		["q" => "Avez-vous entendu parler de la fromagerie qui a explosé en France?", "a" => "Il ne restait plus que de Brie."],
+		["q" => "Notre mariage était si beau", "a" => "Même le gâteau était en étages."],
+		["q" => "Cette piscine est-elle sûre pour la plongée?", "a" => "Elle se termine profondément."],
+		["q" => "Papa, peux-tu mettre mes chaussures?", "a" => "Je ne pense pas qu'elles me conviendront."],
+		["q" => "Est-ce que février mars?", "a" => "Non, mais avril mai"],
+		["q" => "Qu'est-ce qui se trouve au fond de l'océan et qui secoue?", "a" => "Une épave nerveuse."],
+		["q" => "Je lis un livre sur l'histoire de la colle.", "a" => "Je n'arrive tout simplement pas à le poser."],
+		["q" => "Papa, peux-tu mettre le chat dehors?", "a" => "Je ne savais pas qu'il était en feu."],
+		["q" => "Qu'est-ce que l'océan a dit au voilier?", "a" => "Rien, il a juste agité."],
+		["q" => "Qu'est-ce que vous obtenez lorsque vous croisez un bonhomme de neige avec un vampire?", "a" => "Engelures"]
+	]; 
 	
 	
 	// III. Sanitize the `limit` parameter to be sure that it is numeric, and is not too small or large
